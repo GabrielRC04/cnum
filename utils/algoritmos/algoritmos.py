d14777a5d3aa4d1b906014f2162653df63b2e089
GabrielRC04 <131416656+GabrielRC04@users.noreply.github.com>
2026-09-02 21:01:42 +0000

    Semana 5

---
+++ utils/algoritmos/algoritmos.py
@@ -0,0 +1,50 @@
+import numpy as np
+from scipy.optimize import approx_fprime
+
+def bissecao(f,     # função que queremos encontrar a raiz
+              a,    # a início do intervalo
+              b,    # b fim do intervalo
+              TOL,   # erro tolerado
+              iter):  # número máximo de iterações
+    c = (a + b) / 2  # ponto médio entre os valores a e b
+    if f(a) * f(b) > 0:
+        raise ValueError("Nenhuma raiz encontrada no intervalo.")
+    else:
+        i = 0  # variável contador
+        ERRO = abs(f(b) - f(a))  # diferença entre os valores de y
+
+        while ERRO > TOL and i < iter:  # loop iterativo com parada
+            c = (a + b) / 2.0
+            if f(c) == 0:
+                return c, i
+            elif f(a) * f(c) < 0:
+                b = c
+            else:
+                a = c
+            i += 1
+            ERRO = abs(f(b) - f(a))
+        return c, i
+
+def secante(a, b, f, TOL=1e-8):
+    g = lambda a, b: (a * f(b) - b * f(a)) / (f(b) - f(a))
+    x = g(a, b)
+    while abs(x - b) > TOL:
+        a, b = b, x
+        x = g(a, b)
+    return x
+
+def newton_raphson(a, f, TOL=1e-8, df=None):
+    if df is None:
+        def dfn(x):
+            return approx_fprime(np.array([x]), lambda v: f(v[0]))[0]
+    else:
+        dfn = df
+    g = lambda x: x - f(x) / dfn(x)
+    return pontofixo(a, g, TOL)
+
+def pontofixo(a, g, TOL=1e-8):
+    x = g(a)
+    while abs(x - a) > TOL:
+        a = x
+        x = g(a)
+    return x
